f5c3d38d9c425292b0b91f64959fecc933c6e98e
Brayon França <brayonfr@gmail.com>
2021-11-30 11:21:41 -0300

    Progress saving

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const towers = [...document.getElementsByClassName('column')]
 const firstTower = document.getElementById('first-column')
+const thirdTower = document.getElementById('third-column')
+const thirdDisc = document.createElement('div')
+const secondDisc = document.createElement('div')
+const firstDisc = document.createElement('div')
+
+
+
+
+console.log(firstTower)
 console.log(towers)
-const checkChildren = firstTower.childElementCount
-console.log(checkChildren)
+
 
 function createDiscs() {
-    const thirdDisc = document.createElement('div')
     thirdDisc.setAttribute('class', 'third-disc')
     firstTower.appendChild(thirdDisc)
-    const secondDisc = document.createElement('div')
     secondDisc.setAttribute('class', 'second-disc')
     firstTower.appendChild(secondDisc)
-    const firstDisc = document.createElement('div')
     firstDisc.setAttribute('class', 'first-disc')
     firstTower.appendChild(firstDisc)
     console.log(firstDisc)
@@ -27,7 +32,16 @@ console.log(firstDiscTest)
 
 function towersClickHandle(event) {
     let targetEvent = event.currentTarget;
+    let count = targetEvent.childElementCount
+    let whichChild = targetEvent.lastChild
+    thirdTower.appendChild(whichChild)
+    console.log(whichChild)
     console.log(targetEvent)
+    console.log(count)
+    if (thirdTower.childElementCount === 3) {
+
+        alert('You win!')
+    }
 
 }
 
@@ -35,3 +49,11 @@ function towersClickHandle(event) {
 towers.forEach(id => {
     id.addEventListener('click', towersClickHandle)
 })
+
+
+
+
+
+
+// If the first child width of a tower is greater than the first child width 
+// of the other chosen tower, the greatest width disc shan't be moved.
